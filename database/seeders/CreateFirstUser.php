e2281f8805ad27ef1bcbe690d86a8d1d6c89d122
rifqyndaaa <yanda24si@mahasiswa.pcr.ac.id>
2025-11-14 07:46:35 +0700

    belajar seeder

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class CreateFirstUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'thariq@gmail.com',
+            'password' => Hash::make('password12345'),
+        ]);
+    }
+}
